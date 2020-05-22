7d732e6c9b702c2a32e7472d3ef7593e57353abb
Sunny Mattas <sunny.mattas@osudio.com>
2020-05-22 11:56:27 +0200

    encapsulate logic

--- learn/clean/code/MessengerClient.java
+++ learn/clean/code/MessengerClient.java
@@ -5,38 +5,16 @@ public class MessengerClient {
   public static void main(String[] args) {
     String type = args[0];
     String message = args[1];
+    Messenger messanger;
     if ("slack".equals(type)) {
-      if (isValidForSlack(message)) {
-        SlackClient client = new SlackClient();
-        client.send(message);
-      }
+      messanger = new Slack();
     } else if ("skype".equals(type)) {
-
-      if (isValidForSkype(message)) {
-        SlackClient client = new SlackClient();
-        client.send(message);
-      }
+      messanger = new Skype();
     } else if ("hangout".equals(type)) {
-      if (isValidForHangout(message)) {
-        SlackClient client = new SlackClient();
-        client.send(message);
-      }
+      messanger = new Hangout();
     } else {
-      throw new MessageSendFailed(
-          String.format(
-              "This message '%s' cannot be sent to %s. Remove invalid characters", message, type));
+      throw new MessageSendFailed(String.format("This messenger '%s' not supported.", type));
     }
-  }
-
-  private static boolean isValidForHangout(final String message) {
-    return message != null && !message.isBlank() && !message.contains("!");
-  }
-
-  private static boolean isValidForSkype(final String message) {
-    return message != null && !message.isBlank() && !message.contains("<");
-  }
-
-  private static boolean isValidForSlack(final String message) {
-    return message != null && !message.isBlank();
+    messanger.send(message);
   }
 }
--- learn/clean/code/Slack.java
+++ learn/clean/code/Slack.java
@@ -2,8 +2,6 @@ package learn.clean.code;
 
 public class Slack implements Messenger {
 
-  public Messenger
-
   @Override
   public void send(final String message) {
     if (isValidForSlack(message)) {
